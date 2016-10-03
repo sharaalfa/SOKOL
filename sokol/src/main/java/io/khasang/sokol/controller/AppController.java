20396891c860827c2e50b15968225a5a687d561b
AndreyTurubchuk <antur1977@rambler.ru>
2016-10-03 20:01:47 +0300

    удалил маппинги AddRequest, Creator, Performer из AppController

--- sokol/src/main/java/io/khasang/sokol/controller/AppController.java
+++ sokol/src/main/java/io/khasang/sokol/controller/AppController.java
@@ -23,11 +23,6 @@ import java.util.logging.Logger;
 @Controller
 public class AppController {
     private static final Logger log = Logger.getLogger("CreateTable");
-//    @Autowired
-//    private IMessageService messageService;
-
-//    @Autowired
-//    private User user;
 
     @Autowired
     CreateTable createTable;
@@ -35,50 +30,6 @@ public class AppController {
     UserDao userDao;
     @Autowired
     RoleDao roleDao;
-    @Autowired
-    RequestDao requestDao;
-
-    @RequestMapping(value = "/addRequest", method = RequestMethod.GET)
-    public String addRequestPage(Model addRequest){
-        addRequest.addAttribute("addRequest", addRequest);
-        return "addRequest";
-    }
-
-    @RequestMapping(value = "/addRequest", method = RequestMethod.POST)
-    public String addRequest(Model model,  @RequestParam("name") String name, @RequestParam("performer") String performer ){
-       // requestDao.save(request);
-        model.addAttribute("name", name);
-        model.addAttribute("performer", performer);
-        return "hello";
-    }
-
-    @RequestMapping(value = "/addRequestCreator", method = RequestMethod.GET)
-    public String addRequestCreatorPage(Model addRequestCreator){
-        addRequestCreator.addAttribute("addRequestCreator", addRequestCreator);
-        return "addRequestCreator";
-    }
-
-    @RequestMapping(value = "/addRequestCreator", method = RequestMethod.POST)
-    public String addRequestCreator(Model model,  @RequestParam("name") String name, @RequestParam("description") String description ){
-        // requestDao.save(request);
-        model.addAttribute("name", name);
-        model.addAttribute("description", description);
-        return "hello";
-    }
-
-    @RequestMapping(value = "/addRequestPerformer", method = RequestMethod.GET)
-    public String addRequestPerformerPage(Model addRequestPerformer){
-        addRequestPerformer.addAttribute("addRequestPerformer", addRequestPerformer);
-        return "addRequestPerformer";
-    }
-
-    @RequestMapping(value = "/addRequestPerformer", method = RequestMethod.POST)
-    public String addRequestPerformer(Model model,  @RequestParam("name") String name, @RequestParam("description") String description ){
-        // requestDao.save(request);
-        model.addAttribute("name", name);
-        model.addAttribute("description", description);
-        return "hello";
-    }
 
     @RequestMapping("/")
     public String hello(Model model){
