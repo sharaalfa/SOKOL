2eab95c2e4c33ab175d441f1dca85c2c8062f947
AndreyTurubchuk <antur1977@rambler.ru>
2016-10-23 23:47:56 +0300

    сделал передачу типа запроса и фио юзера из requestController во вьюху addRequestCreator через id Типа запроса и id юзера

--- sokol/src/main/java/io/khasang/sokol/dao/UserDao.java
+++ sokol/src/main/java/io/khasang/sokol/dao/UserDao.java
@@ -3,6 +3,7 @@ package io.khasang.sokol.dao;
 import io.khasang.sokol.entity.User;
 
 public interface UserDao extends GenericDao<User> {
+    User getById(Integer userId);
     User getByLogin(String login);
     User getByFio(String fio);
 }
