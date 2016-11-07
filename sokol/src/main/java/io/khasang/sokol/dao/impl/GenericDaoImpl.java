6a6075d00a8aa20ec90cf74af41fd9b8f2394a7a
bMik98 <bmik9898@gmail.com>
2016-11-07 23:39:03 +0300

    new GenericDaoImpl

--- sokol/src/main/java/io/khasang/sokol/dao/impl/GenericDaoImpl.java
+++ sokol/src/main/java/io/khasang/sokol/dao/impl/GenericDaoImpl.java
@@ -5,20 +5,32 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 @Transactional
 public class GenericDaoImpl<T, K extends Serializable> implements GenericDao<T, K> {
-    private Class<T> type;
 
     @Autowired
     private SessionFactory sessionFactory;
 
+    private Class<? extends T> type;
+
     public GenericDaoImpl(Class<T> type) {
         this.type = type;
+    }
+
+    @SuppressWarnings("unchecked")
+    public GenericDaoImpl() {
+        Type t = getClass().getGenericSuperclass();
+        ParameterizedType pt = (ParameterizedType) t;
+        this.type = (Class) pt.getActualTypeArguments()[0];
     }
 
     protected Session getSession() {
@@ -26,7 +38,6 @@ public class GenericDaoImpl<T, K extends Serializable> implements GenericDao<T, 
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public K save(T entity) {
         return (K) getSession().save(entity);
     }
@@ -52,7 +63,6 @@ public class GenericDaoImpl<T, K extends Serializable> implements GenericDao<T, 
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<T> getAll() {
         Query query = getSession().createQuery(String.format("from %s", type.getName()));
         return query.list();
