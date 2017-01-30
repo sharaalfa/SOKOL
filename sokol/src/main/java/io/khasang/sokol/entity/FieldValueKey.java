060d745d72c3ad8ea408309853bbb406446d79c8
bMik98 <bmik9898@gmail.com>
2017-01-31 01:40:50 +0300

    Changed name for class FieldValueKey

--- sokol/src/main/java/io/khasang/sokol/entity/FieldValueKey.java
+++ sokol/src/main/java/io/khasang/sokol/entity/FieldValueKey.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2017 Sokol Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.khasang.sokol.entity;
 
 import javax.persistence.Column;
@@ -11,10 +27,11 @@ public class FileldValueKey implements Serializable {
     @Column(name = "REQUEST_ID")
     private Integer requestId;
 
-    public  FileldValueKey(Integer fieldId,Integer requestId){
+    public FileldValueKey(Integer fieldId, Integer requestId) {
         this.fieldId = fieldId;
         this.requestId = requestId;
     }
+
     public Integer getFieldId() {
         return fieldId;
     }
